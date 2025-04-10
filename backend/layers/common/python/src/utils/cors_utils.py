8c1e6cf84d2b73e336a2f3ff5a40575dda419252
Aldrin Lupisan <lupdrin@gmail.com>
2025-04-10 11:44:29 -0700

    fix: Apply black formatting

--- backend/layers/common/python/src/utils/cors_utils.py
+++ backend/layers/common/python/src/utils/cors_utils.py
@@ -1,14 +1,19 @@
 import os
 
+
 def add_cors_headers(response):
     """Add CORS headers to response"""
     if "headers" not in response:
         response["headers"] = {}
-    
-    response["headers"].update({
-        "Access-Control-Allow-Origin": os.environ.get("CORS_ALLOWED_ORIGIN", "http://localhost:5173"),
-        "Access-Control-Allow-Headers": "Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token",
-        "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS,PATCH",
-        "Access-Control-Allow-Credentials": "true"
-    })
+
+    response["headers"].update(
+        {
+            "Access-Control-Allow-Origin": os.environ.get(
+                "CORS_ALLOWED_ORIGIN", "http://localhost:5173"
+            ),
+            "Access-Control-Allow-Headers": "Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token",
+            "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS,PATCH",
+            "Access-Control-Allow-Credentials": "true",
+        }
+    )
     return response
