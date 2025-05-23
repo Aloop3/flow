7a74eccada26bd5e6d52b0e182583f88255dc068
Aldrin Lupisan <lupdrin@gmail.com>
2025-05-23 09:26:10 -0700

    fix: Update missing import logic for Amplify

--- flow-frontend/amplify/backend.ts
+++ flow-frontend/amplify/backend.ts
@@ -1,11 +1,31 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import * as dotenv from 'dotenv';
 
-/**
- * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
- */
-defineBackend({
-  auth,
-  data,
+dotenv.config();
+
+const backend = defineBackend({
+});
+
+backend.addOutput({
+  auth: {
+    aws_region: 'us-east-1',
+    user_pool_id: process.env.VITE_USER_POOL_ID || '',
+    user_pool_client_id: process.env.VITE_USER_POOL_CLIENT_ID || '',
+    identity_pool_id: undefined,
+    password_policy: {
+      min_length: 8,
+      require_lowercase: true,
+      require_uppercase: true,
+      require_numbers: true,
+      require_symbols: false,
+    },
+  },
+  custom: {
+    API: {
+      flow: {
+        endpoint: process.env.VITE_API_URL || '',
+        region: 'us-east-1',
+      }
+    }
+  }
 });
