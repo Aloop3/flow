905ac56a7ba029bc6992dd96c943e14a0d999552
Aldrin Lupisan <lupdrin@gmail.com>
2025-06-25 14:56:37 -0700

    fix: Correct hypen typo in create_response

--- backend/src/utils/response.py
+++ backend/src/utils/response.py
@@ -25,7 +25,7 @@ def create_response(
         "headers": {
             "Content-Type": "application/json",
             "Access-Control-Allow-Origin": "*",
-            "Access-Control_Allow-Methods": "OPTIONS,GET,POST,PUT,DELETE",
+            "Access-Control-Allow-Methods": "OPTIONS,GET,POST,PUT,DELETE",
             "Access-Control-Allow-Headers": "Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token",
         },
         "body": json.dumps(body, cls=DecimalEncoder),
